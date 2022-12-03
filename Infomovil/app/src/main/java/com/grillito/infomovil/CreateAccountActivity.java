0d1cc3d153dad98bc0f2c66d008be0088912550f
miguel <marcosdelpongo@gmail.com>
2022-12-03 04:45:19 -0400

    Initial commit

---
+++ Infomovil/app/src/main/java/com/grillito/infomovil/CreateAccountActivity.java
@@ -0,0 +1,22 @@
+package com.grillito.infomovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolBar(getString(R.string.tittle_crear_cuenta), true);
+    }
+    public void showToolBar(String titulo, Boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+}
